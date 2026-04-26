a65a1192d8e7c544ccb7900406c09bb35bdac52e
FelipeRamalh0 <felipe.cnramalho@gmail.com>
2026-04-26 00:20:09 -0300

    Importações no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
- 
- 
- 
+import Header from "./Components/Header/Header.jsx";
+import navBar from "./Components/navBar/navBar.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
  
  export default function App() {
 
